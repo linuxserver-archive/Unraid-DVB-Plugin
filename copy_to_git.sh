70708da9431d865ee506030e4030a733c767af6c
Squidly271 <azawadzki@arrowgames.com>
2016-02-13 21:00:00 -0500

    gettin there

---
+++ copy_to_git.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p "/tmp/GitHub/mediabuild/source/mediabuild/usr/local/emhttp/plugins/community.applications/"
+
+cp /usr/local/emhttp/plugins/mediabuild/* /tmp/GitHub/mediabuild/source/mediabuild/usr/local/emhttp/plugins/mediabuild -R -v
+
+
---
+++ source/mediabuild/usr/local/emhttp/plugins/mediabuild/include/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "install shit happens here"
+echo "reboot idiot" > /tmp/mediabuild/reboot
+
+
