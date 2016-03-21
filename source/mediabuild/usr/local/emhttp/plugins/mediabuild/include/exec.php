59a862000489c519a206c5dc575cae3b068fd6de
CHBMB <dr.neil.a.green@gmail.com>
2016-03-21 16:49:42 +0000

    Change Unraid-DVB build repo

--- source/mediabuild/usr/local/emhttp/plugins/mediabuild/include/exec.php
+++ source/mediabuild/usr/local/emhttp/plugins/mediabuild/include/exec.php
@@ -23,7 +23,7 @@ case 'show_description':
 
   $sources = json_decode(file_get_contents($mediaPaths['sources']),true);
 
-  echo "<font size='2' color='red'>".$sources[$build]['imageDescription']."</font>";;
+  echo "<font size='2'>".$sources[$build]['imageDescription']."</font>";;
   break;
 
 #Sets Text to be displayed in Dropdown Menu
@@ -34,7 +34,7 @@ case 'build_buttons':
   $types['stock']    = "unRaid";
 
 
-  $downloadURL = "http://files.linuxserver.io/mediabuild/";
+  $downloadURL = "http://files.linuxserver.io/unraid-dvb/";
   $tempFile = $mediaPaths['tempFiles']."/temp";
   $description = $mediaPaths['tempFiles']."/description";
 
@@ -85,9 +85,10 @@ case 'build_buttons':
             download_url($mediaTypes['imageURL']."/unraid-media",$description);
  
             if ( is_file($description) ) {
-              $mediaTypes['imageDescription'] = file_get_contents($description);
+              $mediaTypes['imageDescription'] = $tempVar = parse_ini_file($description); 
+			  $mediaTypes['imageDescription'] = "This will install the ".$tempVar['base']." DVB build with V".$tempVar['driver']. " drivers";
             } else {
-              $mediaTypes['imageDescription'] = "";
+              $mediaTypes['imageDescription'] = "This will install stock unRAID";
             }
           
             @unlink($description);
