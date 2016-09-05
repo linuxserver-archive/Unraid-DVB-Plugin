7cc23a2ffe271c2a42f6a4a5723269b5d3c1b05f
CHBMB <dr.neil.a.green@gmail.com>
2016-09-05 01:55:59 +0100

    RC5

--- source/UnraidDVB/usr/local/emhttp/plugins/UnraidDVB/include/exec.php
+++ source/UnraidDVB/usr/local/emhttp/plugins/UnraidDVB/include/exec.php
@@ -148,7 +148,7 @@ case 'build_buttons':
     }
     $i = ++$i;
   }
-  $o = "<center>";
+$o = "<center>";
   foreach ( $buttons as $button )
   {
     if ( $button['name'] == "unRaid" )
@@ -165,7 +165,6 @@ case 'build_buttons':
     }
     $o .= "</select>";
   }
-  $o .= "</center>";
   echo $o;
   break;
 
