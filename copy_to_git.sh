65e13ce84d2ceee58ef5edc6503c5d7107a9b66a
chbmb <chbmb@linuxserver.io>
2018-03-12 12:16:30 +0000

    Update copy_to_git.sh

--- copy_to_git.sh
+++ copy_to_git.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mkdir -p "//mnt/cache/.appdata/github/Unraid-DVB-Plugin/source/UnraidDVB/usr/local/emhttp/plugins/UnraidDVB/"
+mkdir -p "/mnt/disk1/github/Unraid-DVB-Plugin/source/UnraidDVB/usr/local/emhttp/plugins/UnraidDVB/"
 
 cp /usr/local/emhttp/plugins/UnraidDVB/* /mnt/disk1/github/Unraid-DVB-Plugin/source/UnraidDVB/usr/local/emhttp/plugins/UnraidDVB/ -R -v
 
